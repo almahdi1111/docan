f075ea7da1c0894a1ddbcefa93b2a155d3a351cd
almahdi1111 <78163384+almahdi1111@users.noreply.github.com>
2021-01-28 19:22:17 +0300

    Add files via upload

---
+++ assets/js/Profile-Edit-Form.js
@@ -0,0 +1,28 @@
+(function(){
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar-bg').css({
+                    'background':'url('+e.target.result+')',
+                    'background-size':'cover',
+                    'background-position': '50% 50%'
+                });
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    
+
+    $("input.form-control[name=avatar-file]").change(function(){
+        readURL(this);
+    });
+    
+    
+    
+ 
+})()
